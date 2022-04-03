f53546a09935479d785e54a7c08443715defa2e5
Konstantin Plisov <3dnow@bk.ru>
2022-04-03 13:25:26 +0500

    allure config

--- src/test/java/work/selenium/utils/Constants.java
+++ src/test/java/work/selenium/utils/Constants.java
@@ -2,5 +2,5 @@ package work.selenium.utils;
 
 public abstract class Constants {
     public static final int EXPLICIT_WAIT_TIME = 7;
-    public static final int IMPLICIT_WAIT_TIME = 0;
+    public static final int IMPLICIT_WAIT_TIME = 5;
 }
